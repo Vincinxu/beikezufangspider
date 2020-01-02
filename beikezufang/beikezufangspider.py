9dbaca9024b80d285363875e62f9befaa6a76828
Vincinxu <53092954+Vincinxu@users.noreply.github.com>
2020-01-02 17:43:35 +0800

    Update beikezufangspider.py

--- beikezufang/beikezufangspider.py
+++ beikezufang/beikezufangspider.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 
 class BkSpider():
+    #初始化
     def __init__(self, keyword, max_page, mysql_host,
                  mysql_user, mysql_password, mysql_db, mysql_port):
         self.keyword = keyword
@@ -15,6 +16,7 @@ class BkSpider():
         self.mysql_db = mysql_db
         self.mysql_port = mysql_port
 
+     #获取页面   
     def get_page(self, page):
         try:
             url = 'https://' + self.keyword + '.zu.ke.com/zufang/pg' + str(page)
@@ -27,7 +29,8 @@ class BkSpider():
             return None
         except RequestException:
             return None
-
+    
+    #解析页面，提取数据
     def parse_page(self, html):
         bs = BeautifulSoup(html, 'lxml')
         items = bs.select('.content__list .content__list--item')
@@ -42,7 +45,7 @@ class BkSpider():
             }
             yield product
 
-
+    #保存到MySQL
     def save_to_mysql(self, content):
         self.db = pymysql.connect(self.mysql_host, self.mysql_user, self.mysql_password,
                                   self.mysql_db, self.mysql_port, charset='utf8')
@@ -58,11 +61,13 @@ class BkSpider():
         except:
             print('保存失败')
             self.db.rollback()
-
+    
+    
+    #关闭数据库
     def close_sql(self):
         self.db.close()
 
-
+    #运行
     def run(self):
         for i in range(1, self.max_page + 1):
             html = self.get_page(i)
